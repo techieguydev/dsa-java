73f23f72c7108b4753e4f2ef8098398cb0cbb42d
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-02 01:48:16 +0100

    Stack from list code

--- src/main/java/dev/javatechie/stack/StackFromList.java
+++ src/main/java/dev/javatechie/stack/StackFromList.java
@@ -1,4 +1,47 @@
 package dev.javatechie.stack;
 
-public class StackFromList {
+import java.util.LinkedList;
+
+public class StackFromList<T> {
+
+    public static void main(String[] args) {
+        final StackFromList<String> stack = new StackFromList<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+
+        System.out.println(stack.pop());
+        System.out.println("==== Print Stack ====");
+        stack.printStack();
+
+    }
+
+    private LinkedList<T> list = new LinkedList<>();
+
+    public void push(final T item) {
+        list.addFirst(item);
+    }
+
+    public T pop() {
+        return list.removeFirst();
+    }
+
+    public T peek() {
+        return list.getFirst();
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void printStack() {
+        for(T item : list) {
+            System.out.println(item);
+        }
+    }
 }
