efdc7453eba629363a8f278d526621d3d6140ddc
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-04 00:52:55 +0100

    LinkedList based stack complete implementation udpated code

--- src/main/java/dev/javatechie/stack/LinkedListBasedStack.java
+++ src/main/java/dev/javatechie/stack/LinkedListBasedStack.java
@@ -1,12 +1,23 @@
 package dev.javatechie.stack;
 
+import java.util.NoSuchElementException;
+
 public class LinkedListBasedStack {
 
     public static void main(String[] args) {
+        final LinkedListBasedStack linkedListStack = new LinkedListBasedStack();
+        //linkedListStack.peek();
+        linkedListStack.push("item1");
+        linkedListStack.push("item2");
+        linkedListStack.push("item3");
 
+        linkedListStack.printStack();
+        System.out.println(linkedListStack.size());
+        System.out.println(linkedListStack.pop());
+        System.out.println(linkedListStack.size());
     }
 
-    private class Node {
+    private static class Node {
         private Object item;
         private Node next;
 
@@ -17,9 +28,11 @@ public class LinkedListBasedStack {
     }
 
     private Node top = null;
+    private int size = 0;
 
     public void push(final Object item) {
         top = new Node(item, top);
+        size++;
     }
 
     public Object pop() {
@@ -28,6 +41,28 @@ public class LinkedListBasedStack {
         }
         final Object item = peek();
         top = top.next;
+        size--;
         return item;
+    }
+
+    public Object peek() {
+        if (top == null) {
+            throw new NoSuchElementException("Empty stack");
+        }
+        return top.item;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void printStack() {
+        for (Node node = top; node != null; node = node.next) {
+            System.out.println(node.item);
+        }
     }
 }
