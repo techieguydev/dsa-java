3204c8f3922ede5538f4df853d7c5242f0afbe19
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-04 00:00:47 +0100

    LinkedList based stack implementation udpated code

--- src/main/java/dev/javatechie/stack/LinkedListBasedStack.java
+++ src/main/java/dev/javatechie/stack/LinkedListBasedStack.java
@@ -6,4 +6,28 @@ public class LinkedListBasedStack {
 
     }
 
+    private class Node {
+        private Object item;
+        private Node next;
+
+        public Node(final Object item, final Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node top = null;
+
+    public void push(final Object item) {
+        top = new Node(item, top);
+    }
+
+    public Object pop() {
+        if (top == null) {
+            throw new IllegalStateException("Empty stack");
+        }
+        final Object item = peek();
+        top = top.next;
+        return item;
+    }
 }
