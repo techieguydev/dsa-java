2af797ecc592a0066cd4a56983f8ec60037edb97
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-05 02:03:39 +0100

    Array based stack - added javadoc

--- src/main/java/dev/javatechie/stack/ArrayBasedStack.java
+++ src/main/java/dev/javatechie/stack/ArrayBasedStack.java
@@ -2,8 +2,16 @@ package dev.javatechie.stack;
 
 import java.util.NoSuchElementException;
 
+/**
+ * The type Array based stack.
+ */
 public class ArrayBasedStack {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         ArrayBasedStack stack = new ArrayBasedStack(10);
         stack.push("Item1");
@@ -29,10 +37,20 @@ public class ArrayBasedStack {
 
     private int size = 0;
 
+    /**
+     * Instantiates a new Array based stack.
+     *
+     * @param capacity the capacity
+     */
     public ArrayBasedStack(int capacity) {
         items = new Object[capacity];
     }
 
+    /**
+     * Push.
+     *
+     * @param item the item
+     */
     public void push(Object item) {
         if (size == items.length) {
             throw new IllegalStateException("We can't insert item anymore");
@@ -40,6 +58,11 @@ public class ArrayBasedStack {
         items[size++] = item;
     }
 
+    /**
+     * Pop object.
+     *
+     * @return the object
+     */
     public Object pop() {
         if (size == 0) {
             throw new NoSuchElementException("No item present in stack");
@@ -50,6 +73,11 @@ public class ArrayBasedStack {
         return item;
     }
 
+    /**
+     * Peek object.
+     *
+     * @return the object
+     */
     public Object peek() {
         if (size == 0) {
             throw new NoSuchElementException("Empty stack");
@@ -57,14 +85,27 @@ public class ArrayBasedStack {
         return items[size - 1];
     }
 
+    /**
+     * Size int.
+     *
+     * @return the int
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Is empty boolean.
+     *
+     * @return the boolean
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Print stack items.
+     */
     public void printStackItems() {
         for (final Object item : items) {
             if(item != null)
