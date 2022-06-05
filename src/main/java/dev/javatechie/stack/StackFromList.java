5ba57e06fafb070dcc73e0534b38182a412876fc
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-05 02:04:44 +0100

    List based stack - added javadoc

--- src/main/java/dev/javatechie/stack/StackFromList.java
+++ src/main/java/dev/javatechie/stack/StackFromList.java
@@ -2,8 +2,18 @@ package dev.javatechie.stack;
 
 import java.util.LinkedList;
 
+/**
+ * The type Stack from list.
+ *
+ * @param <T> the type parameter
+ */
 public class StackFromList<T> {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         final StackFromList<String> stack = new StackFromList<>();
         stack.push("A");
@@ -19,26 +29,54 @@ public class StackFromList<T> {
 
     private LinkedList<T> list = new LinkedList<>();
 
+    /**
+     * Push.
+     *
+     * @param item the item
+     */
     public void push(final T item) {
         list.addFirst(item);
     }
 
+    /**
+     * Pop t.
+     *
+     * @return the t
+     */
     public T pop() {
         return list.removeFirst();
     }
 
+    /**
+     * Peek t.
+     *
+     * @return the t
+     */
     public T peek() {
         return list.getFirst();
     }
 
+    /**
+     * Gets size.
+     *
+     * @return the size
+     */
     public int getSize() {
         return list.size();
     }
 
+    /**
+     * Is empty boolean.
+     *
+     * @return the boolean
+     */
     public boolean isEmpty() {
         return list.isEmpty();
     }
 
+    /**
+     * Print stack.
+     */
     public void printStack() {
         for(T item : list) {
             System.out.println(item);
