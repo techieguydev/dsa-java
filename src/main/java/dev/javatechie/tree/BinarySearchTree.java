70a7027e1b1abb6fcac818b133f3956659f1cc34
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-08 23:51:32 +0100

    Binary search tree implementation

---
+++ src/main/java/dev/javatechie/tree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package dev.javatechie.tree;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+
+    public static void main(String[] args) {
+
+    }
+
+    private class Node<T> {
+        private
+    }
+}
