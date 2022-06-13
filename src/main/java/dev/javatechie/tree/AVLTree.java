93ac2c8857466dfd22ceb80c35dc4e346607f18d
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-13 23:58:05 +0100

    AVL Tree updated code

--- src/main/java/dev/javatechie/tree/AVLTree.java
+++ src/main/java/dev/javatechie/tree/AVLTree.java
@@ -23,7 +23,9 @@ public class AVLTree<E> {
      *
      * */
     private class Node<T> {
+        //
         private T data;
+        //
         private Node<T> left;
         private Node<T> right;
 
