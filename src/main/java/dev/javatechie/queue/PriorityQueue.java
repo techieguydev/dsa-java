3604c9af78e24ea63d85dc279b0fcbfaa4e32b78
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-14 23:43:22 +0100

    PriorityQueue initial code

---
+++ src/main/java/dev/javatechie/queue/PriorityQueue.java
@@ -0,0 +1,14 @@
+package dev.javatechie.queue;
+
+public class PriorityQueue<E extends Comparable<E>> {
+    public static void main(String[] args) {
+
+    }
+
+    private class Node<T> {
+        private T data;
+        private Node next;
+
+
+    }
+}
