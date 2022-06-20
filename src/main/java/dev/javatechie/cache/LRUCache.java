a4ef50403d3e59d2ce60a3cd94869a2b2f0f36f2
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-20 23:22:55 +0100

    LRU Cache updated code

--- src/main/java/dev/javatechie/cache/LRUCache.java
+++ src/main/java/dev/javatechie/cache/LRUCache.java
@@ -8,5 +8,7 @@ public class LRUCache<K, V> {
     private final Map<K, Map.Entry<K, V>> data = new HashMap<>();
     private Map.Entry<K, V> head;
     private Map.Entry<K, V> tail;
+    private int cap;
+    private static final int DEFAULT_CAP = 100;
 
 }
