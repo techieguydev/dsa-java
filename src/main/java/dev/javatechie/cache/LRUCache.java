08e0954455f6cc930b29365064eb55465b34fc2f
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-20 23:22:08 +0100

    LRU Cache updated code

--- src/main/java/dev/javatechie/cache/LRUCache.java
+++ src/main/java/dev/javatechie/cache/LRUCache.java
@@ -6,5 +6,7 @@ import java.util.Map;
 public class LRUCache<K, V> {
 
     private final Map<K, Map.Entry<K, V>> data = new HashMap<>();
+    private Map.Entry<K, V> head;
+    private Map.Entry<K, V> tail;
 
 }
