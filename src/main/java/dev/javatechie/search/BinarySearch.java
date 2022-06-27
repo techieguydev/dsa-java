97fa7a78d06ee0fc6ba9d9af5abe7b4e42a16a5b
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-27 23:35:41 +0100

    Binary search: updated code

--- src/main/java/dev/javatechie/search/BinarySearch.java
+++ src/main/java/dev/javatechie/search/BinarySearch.java
@@ -1,6 +1,14 @@
 package dev.javatechie.search;
 
+/**
+ * The type Binary search.
+ */
 public class BinarySearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12, 15};
         int target = 7;
@@ -8,6 +16,13 @@ public class BinarySearch {
         System.out.println(bs.binarySearch(nums, target));
     }
 
+    /**
+     * Binary search int.
+     *
+     * @param nums   the nums
+     * @param target the target
+     * @return the int
+     */
     public int binarySearch(final int[] nums, final int target) {
         int left = 0;
         int right = nums.length - 1;
