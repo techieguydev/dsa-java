3addc8f0104ae52e074ef4114e21cdc6f338bbc8
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-27 23:35:10 +0100

    Sorting: updated code

--- src/main/java/dev/javatechie/sort/Sorting.java
+++ src/main/java/dev/javatechie/sort/Sorting.java
@@ -1,6 +1,14 @@
 package dev.javatechie.sort;
 
+/**
+ * The type Sorting.
+ */
 public class Sorting {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("==== Basic sorting ====");
     }
