0a783bcfd0e259f25674aa915a56a89e79d4ec39
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-30 12:59:36 +0100

    Graph: updated code

--- src/main/java/dev/javatechie/graph/Graph.java
+++ src/main/java/dev/javatechie/graph/Graph.java
@@ -1,6 +1,11 @@
 package dev.javatechie.graph;
 
 public class Graph {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
