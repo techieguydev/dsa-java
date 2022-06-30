f269738d006fd1a30f89baad5515d68c3601c141
Qadir Hussain <qadir.hussain99@gmail.com>
2022-06-30 12:58:55 +0100

    Hashing: updated code

--- src/main/java/dev/javatechie/hashing/Hashing.java
+++ src/main/java/dev/javatechie/hashing/Hashing.java
@@ -1,6 +1,11 @@
 package dev.javatechie.hashing;
 
 public class Hashing {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
