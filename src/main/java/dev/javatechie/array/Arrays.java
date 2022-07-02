db39a7470d3c50e023af038e00721b3715b4e81f
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-02 11:04:06 +0100

    Arrays: updated code

--- src/main/java/dev/javatechie/array/Arrays.java
+++ src/main/java/dev/javatechie/array/Arrays.java
@@ -1,6 +1,11 @@
 package dev.javatechie.array;
 
 public class Arrays {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
