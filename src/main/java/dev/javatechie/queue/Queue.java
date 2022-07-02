60c9225f96631016db83c885acb8846f3d49b38b
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-02 11:05:58 +0100

    Queue: updated code

--- src/main/java/dev/javatechie/queue/Queue.java
+++ src/main/java/dev/javatechie/queue/Queue.java
@@ -1,6 +1,11 @@
 package dev.javatechie.queue;
 
 public class Queue {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("==== Basic Queue ====");
     }
