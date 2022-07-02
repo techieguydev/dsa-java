792299027991900887689cee47a29502a62b128c
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-02 11:04:58 +0100

    PriorityQueue: updated code

--- src/main/java/dev/javatechie/queue/PriorityQueue.java
+++ src/main/java/dev/javatechie/queue/PriorityQueue.java
@@ -1,6 +1,11 @@
 package dev.javatechie.queue;
 
 public class PriorityQueue<E extends Comparable<E>> {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
