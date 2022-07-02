82f7420115c353a5ac3b5571778f77f51e2f58ff
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-02 11:07:52 +0100

    Searching: updated code

--- src/main/java/dev/javatechie/search/Searching.java
+++ src/main/java/dev/javatechie/search/Searching.java
@@ -1,6 +1,11 @@
 package dev.javatechie.search;
 
 public class Searching {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("==== Basic searching ====");
     }
