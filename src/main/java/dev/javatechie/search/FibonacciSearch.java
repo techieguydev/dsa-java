5607981ea0f24fc2e9bbcfe72f389601747e13f4
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-03 18:05:25 +0100

    Fibonacci Searching updated code

--- src/main/java/dev/javatechie/search/FibonacciSearch.java
+++ src/main/java/dev/javatechie/search/FibonacciSearch.java
@@ -10,6 +10,6 @@ public class FibonacciSearch {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("Fibonacci Search starting");
     }
 }
