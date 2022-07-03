656c2ae78b3a3df97f775758ba60f7931d759f03
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-03 18:04:58 +0100

    Fibonacci Searching updated code

--- src/main/java/dev/javatechie/search/FibonacciSearch.java
+++ src/main/java/dev/javatechie/search/FibonacciSearch.java
@@ -1,6 +1,14 @@
 package dev.javatechie.search;
 
+/**
+ * The type Fibonacci search.
+ */
 public class FibonacciSearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
