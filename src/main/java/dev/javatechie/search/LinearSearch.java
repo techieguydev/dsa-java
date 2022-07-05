0afe36af48dc3fdd5b07b9b6ee5a0fce3f33f8d2
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-05 18:58:15 +0100

    Jump Searching updated code

--- src/main/java/dev/javatechie/search/LinearSearch.java
+++ src/main/java/dev/javatechie/search/LinearSearch.java
@@ -1,6 +1,11 @@
 package dev.javatechie.search;
 
 public class LinearSearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
