4d4a6eb4fb8816ca31b2e96808a955d54dffde74
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-05 18:57:49 +0100

    Jump Searching updated code

--- src/main/java/dev/javatechie/search/JumpSearch.java
+++ src/main/java/dev/javatechie/search/JumpSearch.java
@@ -1,6 +1,11 @@
 package dev.javatechie.search;
 
 public class JumpSearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
