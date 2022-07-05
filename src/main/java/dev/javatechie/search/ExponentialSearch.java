cbd11cfcb411ca241a4133d80ad40684699e6b59
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-05 19:00:11 +0100

    Exponential Searching updated code

--- src/main/java/dev/javatechie/search/ExponentialSearch.java
+++ src/main/java/dev/javatechie/search/ExponentialSearch.java
@@ -1,6 +1,11 @@
 package dev.javatechie.search;
 
 public class ExponentialSearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
