a461db1cb5706313139c343d6bb919255a1b8e61
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-06 18:20:05 +0100

    Interpolation Search: update code

--- src/main/java/dev/javatechie/search/InterpolationSearch.java
+++ src/main/java/dev/javatechie/search/InterpolationSearch.java
@@ -1,6 +1,11 @@
 package dev.javatechie.search;
 
 public class InterpolationSearch {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
