fa49a5ac586177cfd38a8dfebce4662ca8a1c9d3
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-09 17:03:50 +0100

    Binary Heap: updated code

--- src/main/java/dev/javatechie/heap/BinaryHeap.java
+++ src/main/java/dev/javatechie/heap/BinaryHeap.java
@@ -1,6 +1,11 @@
 package dev.javatechie.heap;
 
 public class BinaryHeap {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
