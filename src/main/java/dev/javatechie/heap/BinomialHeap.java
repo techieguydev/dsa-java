e7515784cf519c4eb7614451b3fb9722fc74e37e
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-09 17:05:31 +0100

    Binomial Heap: updated code

--- src/main/java/dev/javatechie/heap/BinomialHeap.java
+++ src/main/java/dev/javatechie/heap/BinomialHeap.java
@@ -1,6 +1,11 @@
 package dev.javatechie.heap;
 
 public class BinomialHeap {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
