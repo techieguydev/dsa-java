42f957e701c1aea971d2920b12d4973539123c93
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-11 09:13:03 +0100

    Leftist Heap: updated code

--- src/main/java/dev/javatechie/heap/LeftistHeap.java
+++ src/main/java/dev/javatechie/heap/LeftistHeap.java
@@ -1,6 +1,11 @@
 package dev.javatechie.heap;
 
 public class LeftistHeap {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
