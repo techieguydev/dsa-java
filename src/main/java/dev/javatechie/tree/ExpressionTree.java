4f883507f1ca5d126892c0b819e96a8544aa09e1
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-14 18:23:38 +0100

    Expression: Updated code

--- src/main/java/dev/javatechie/tree/ExpressionTree.java
+++ src/main/java/dev/javatechie/tree/ExpressionTree.java
@@ -1,6 +1,11 @@
 package dev.javatechie.tree;
 
 public class ExpressionTree {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("Expression tree starts");
     }
