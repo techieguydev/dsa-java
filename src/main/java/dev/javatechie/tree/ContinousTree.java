a80748fafef7c6fa5b272ca8f3c6b3d22ba4bb70
Qadir Hussain <qadir.hussain99@gmail.com>
2022-07-16 18:54:06 +0100

    Continous Tree: Updated code

--- src/main/java/dev/javatechie/tree/ContinousTree.java
+++ src/main/java/dev/javatechie/tree/ContinousTree.java
@@ -1,6 +1,11 @@
 package dev.javatechie.tree;
 
 public class ContinousTree {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
 
     }
