a007adbe87f74f05568de96eca87e0539f7d3e94
Qadir Hussain <qadir.hussain99@gmail.com>
2022-09-29 22:58:01 +0100

    ListBased Stack: Updated code

--- src/main/java/dev/javatechie/stack/StackFromList.java
+++ src/main/java/dev/javatechie/stack/StackFromList.java
@@ -15,7 +15,7 @@ public class StackFromList<T> {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        System.out.println("=== List based");
+        System.out.println("=== List based stack ");
         final StackFromList<String> stack = new StackFromList<>();
         stack.push("A");
         stack.push("B");
