108beebef824aafe5ff82e38a5651e17e8443aa7
Qadir Hussain <qadir.hussain99@gmail.com>
2022-10-12 22:09:09 +0100

    Binary Tree: Updated code

--- src/main/java/dev/javatechie/tree/BinaryTree.java
+++ src/main/java/dev/javatechie/tree/BinaryTree.java
@@ -13,7 +13,7 @@ public class BinaryTree<E extends Comparable<E>> {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("===");
     }
 
     private Node<E> root;
