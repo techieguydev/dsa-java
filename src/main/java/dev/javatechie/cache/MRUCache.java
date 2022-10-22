d08548449cd9f6aa3ef6ca50d964940ea6f42c0f
Qadir Hussain <qadir.hussain99@gmail.com>
2022-10-22 20:51:20 +0100

    MRU Cache: Updated code - added add node with frequency code

--- src/main/java/dev/javatechie/cache/MRUCache.java
+++ src/main/java/dev/javatechie/cache/MRUCache.java
@@ -23,6 +23,7 @@ public class MRUCache<K, V> {
         }
         this.cap = newCapacity;
     }
+
     private void checkCapacity(int capacity) {
         if(capacity <= 0) {
             throw new RuntimeException("Capacity must be greater than zero!");
