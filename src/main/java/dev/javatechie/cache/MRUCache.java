c6d5918a2c9e79573cd196431ac0b264616f6567
Qadir Hussain <qadir.hussain99@gmail.com>
2022-10-23 22:00:41 +0100

    MRU Cache: Updated code - added add node with frequency code

--- src/main/java/dev/javatechie/cache/MRUCache.java
+++ src/main/java/dev/javatechie/cache/MRUCache.java
@@ -37,5 +37,6 @@ public class MRUCache<K, V> {
         final Entry<K, V> evicted = this.tail;
         tail = evicted.getPreEntry();
         tail.setNextEntry(null);
+        evicted.setNextEntry(null);
     }
 }
