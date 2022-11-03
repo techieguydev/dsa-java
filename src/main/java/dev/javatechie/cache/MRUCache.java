963b41ed2300ac47347ac82dcd69b821d740e1f5
Qadir Hussain <qadir.hussain99@gmail.com>
2022-11-03 21:24:29 +0000

    MRU Cache: Updated code - added new entry final class code

--- src/main/java/dev/javatechie/cache/MRUCache.java
+++ src/main/java/dev/javatechie/cache/MRUCache.java
@@ -89,6 +89,7 @@ public class MRUCache<K, V> {
         if(nextEntry != null) {
             nextEntry.setPreEntry(nextEntry);
         }
+        if
     }
 
     private void addNewEntry(final Entry<K, V> entry) {
