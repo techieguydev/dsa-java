2860089ae655c3bfe7706b73317f2c0000d2941f
Qadir Hussain <qadir.hussain99@gmail.com>
2022-11-17 20:11:41 +0000

    Adjacency Graph: Updated code

--- src/main/java/dev/javatechie/graph/AdjacencyListGraph.java
+++ src/main/java/dev/javatechie/graph/AdjacencyListGraph.java
@@ -72,6 +72,7 @@ public class AdjacencyListGraph<E extends Comparable<E>> {
         }
         if(fromV == null) {
             fromV = new Vertex(from);
+            vertices.add(toV);
         }
     }
 }
