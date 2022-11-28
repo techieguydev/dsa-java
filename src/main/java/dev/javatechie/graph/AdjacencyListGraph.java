3a4e726267b6f65a0be532587a5f25e78a996264
Qadir Hussain <qadir.hussain99@gmail.com>
2022-11-28 23:03:34 +0000

    Adjacency Graph: Updated code

--- src/main/java/dev/javatechie/graph/AdjacencyListGraph.java
+++ src/main/java/dev/javatechie/graph/AdjacencyListGraph.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class creates Adjacency list graph which accepts a generic class .
+ * This class creates Adjacency list graph which accepts a generic class object.
  *
  * @param <E> the type parameter
  */
