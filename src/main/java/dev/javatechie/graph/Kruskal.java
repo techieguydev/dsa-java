b2e6e4e9861198cf47cd0b96bbd313e76049ebc2
Qadir Hussain <qadir.hussain99@gmail.com>
2022-11-29 22:09:01 +0000

    Kruskal Graph: Adding edge constructor - updated code

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -10,6 +10,7 @@ public class Kruskal {
         public Edge(int from, int to, int weight) {
             this.from = from;
             this.to = to;
+            this.weight = weight;
         }
     }
 }
