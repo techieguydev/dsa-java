e81a0b6ec4d4fed14b413b2ab175050470959336
Qadir Hussain <qadir.hussain99@gmail.com>
2022-12-08 23:35:43 +0000

    Kruskal Graph: Updating main function

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -48,6 +48,8 @@ public class Kruskal {
         HashSet<Edge>[] solGraph = kruskal.kruskal(graph);
 
         System.out.println("Minimal Graph: ");
-        for (int i = 0; i < solGraph.length;)
+        for (int i = 0; i < solGraph.length; i++) {
+
+        }
     }
 }
