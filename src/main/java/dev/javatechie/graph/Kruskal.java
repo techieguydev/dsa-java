edde4cd6b21c29efe9b30bb0c475bf211788b896
Qadir Hussain <qadir.hussain99@gmail.com>
2022-12-12 22:14:20 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -69,6 +69,7 @@ public class Kruskal {
             connectedGroups[i] = new HashSet<>();
             connectedGroups[i].add(i);
             captain[i] = i;
+            edges
         }
     }
 }
