ad7d78d27a2a9cd9a4a90585b93b8ecfddf4873d
Qadir Hussain <qadir.hussain99@gmail.com>
2022-12-15 23:18:30 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -75,7 +75,7 @@ public class Kruskal {
         int connectedElements = 0;
         while(connectedElements != nodes && !edges.isEmpty()) {
             final Edge edge = edges.poll();
-            if() {
+            if(!connectedGroups[[edge.from]]) {
 
             }
         }
