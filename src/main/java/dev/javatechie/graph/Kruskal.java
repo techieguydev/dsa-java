60f6d32f17cc70a2a2e5878515b4e0f78ef3644a
Qadir Hussain <qadir.hussain99@gmail.com>
2022-12-18 22:06:31 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -78,7 +78,7 @@ public class Kruskal {
             if(!connectedGroups[captain[edge.from]].contains(edge.to) && !connectedGroups[captain[edge.to]].contains(edge.from)) {
                 connectedGroups[captain[edge.from]].addAll(connectedGroups[captain[edge.to]]);
                 connectedGroups[captain[edge.from]].forEach(i -> captain[i] = captain[edge.from]);
-                addEdge(minGraph, edge.from, );
+                addEdge(minGraph, edge.from, edge.to);
             }
         }
     }
