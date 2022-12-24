a4e02c8c293422161b746768ac3fbc6e5bac10ee
Qadir Hussain <qadir.hussain99@gmail.com>
2022-12-24 23:52:52 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Kruskal.java
+++ src/main/java/dev/javatechie/graph/Kruskal.java
@@ -75,7 +75,7 @@ public class Kruskal {
     /**
      * Kruskal main function.
      *
-     * @param graph the HashSet
+     * @param graph the HashSet array
      * @return the array of HashSet object
      */
     public HashSet<Edge>[] kruskal(final HashSet<Edge>[] graph) {
