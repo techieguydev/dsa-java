34e56a26639ac9ef925fc7961d01804f5c7a2fd3
Qadir Hussain <qadir.hussain99@gmail.com>
2023-01-07 23:48:48 +0000

    Dijkstras algorithm: Updated code

--- src/main/java/dev/javatechie/graph/Dijkstras.java
+++ src/main/java/dev/javatechie/graph/Dijkstras.java
@@ -50,7 +50,7 @@ public class Dijkstras {
 
         for(int i = 0; i < k; i++) {
             dist[i] = Integer.MAX_VALUE;
-            set[i] = i;
+            set[i] = false;
         }
     }
 }
