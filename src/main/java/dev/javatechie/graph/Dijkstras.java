0cbfdd3cf3e58a110cd9144e09818a5da60e1039
Qadir Hussain <qadir.hussain99@gmail.com>
2023-01-12 23:43:52 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Dijkstras.java
+++ src/main/java/dev/javatechie/graph/Dijkstras.java
@@ -12,7 +12,7 @@ public class Dijkstras {
      *
      * @param dist the array of dist
      * @param set  the set of array
-     * @return the int
+     * @return the
      */
     int minDist(int dist[], Boolean set[]) {
         int min = Integer.MAX_VALUE;
