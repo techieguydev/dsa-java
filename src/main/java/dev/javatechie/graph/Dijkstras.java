497ec9a0ef49a56009565fb4f9bbc8b126c8dafe
Qadir Hussain <qadir.hussain99@gmail.com>
2023-01-12 23:43:14 +0000

    Kruskal Graph: Updated main method

--- src/main/java/dev/javatechie/graph/Dijkstras.java
+++ src/main/java/dev/javatechie/graph/Dijkstras.java
@@ -8,7 +8,7 @@ public class Dijkstras {
     int k = 0;
 
     /**
-     * Return Minimum dist in.
+     * Return Minimum dist in number.
      *
      * @param dist the array of dist
      * @param set  the set
